8777610f0122522ddd33e29d49c23c6e8d3a0ba7
huwang <windang@163.com>
2017-07-20 20:39:00 +0800

    全屏模式

--- statusbardemo/src/main/java/com/example/huwang/statusbardemo/MainActivity.java
+++ statusbardemo/src/main/java/com/example/huwang/statusbardemo/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
